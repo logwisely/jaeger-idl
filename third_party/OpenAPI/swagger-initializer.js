b397eafdc6ec4461ea9c14bc1a58d6e784ffd2e0
LogWisely <logwisely@gmail.com>
2026-02-25 20:32:31 -0800

    Add a new grpc proto service for extended capabilities about attributes in storage polugin

--- third_party/OpenAPI/swagger-initializer.js
+++ third_party/OpenAPI/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    urls: [{"url":"zipkin.swagger.json","name":"zipkin.swagger.json"},{"url":"storage/v2/trace_storage.swagger.json","name":"storage/v2/trace_storage.swagger.json"},{"url":"storage/v2/dependency_storage.swagger.json","name":"storage/v2/dependency_storage.swagger.json"},{"url":"api_v2/query.swagger.json","name":"api_v2/query.swagger.json"},{"url":"api_v2/sampling.swagger.json","name":"api_v2/sampling.swagger.json"},{"url":"api_v2/model.swagger.json","name":"api_v2/model.swagger.json"},{"url":"api_v2/collector.swagger.json","name":"api_v2/collector.swagger.json"},{"url":"api_v3/query_service.swagger.json","name":"api_v3/query_service.swagger.json"}],
+    urls: [{"url":"zipkin.swagger.json","name":"zipkin.swagger.json"},{"url":"storage/v2/trace_storage.swagger.json","name":"storage/v2/trace_storage.swagger.json"},{"url":"storage/v2/dependency_storage.swagger.json","name":"storage/v2/dependency_storage.swagger.json"},{"url":"storage/v2/attributes_storage.swagger.json","name":"storage/v2/attributes_storage.swagger.json"},{"url":"api_v2/query.swagger.json","name":"api_v2/query.swagger.json"},{"url":"api_v2/sampling.swagger.json","name":"api_v2/sampling.swagger.json"},{"url":"api_v2/model.swagger.json","name":"api_v2/model.swagger.json"},{"url":"api_v2/collector.swagger.json","name":"api_v2/collector.swagger.json"},{"url":"api_v3/query_service.swagger.json","name":"api_v3/query_service.swagger.json"}],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
